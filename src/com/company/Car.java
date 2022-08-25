798bce8902a20e5096ed9a177104cf8175c541d6
Artem Kovalov <artemkov1990@gmail.com>
2022-08-25 15:42:33 +0300

    First commit

---
+++ src/com/company/Car.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Car {
+
+    public void start() {
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+    }
+
+    private void startFuelSystem () {
+    }
+
+    private void startCommand () {
+    }
+
+    private void startElectricity () {
+    }
+}
